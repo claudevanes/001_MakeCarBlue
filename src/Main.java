7b35ac7e8e99cd0efd10f417fc56692bb2a0c02a
claudevanes <dvorskakamila@gmail.com>
2016-05-09 15:44:30 +0200

    00? This project is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.logging.Logger;
 
+import colours.*;
+import garage.*;
+
 public class Main {
 
 	public Main() {
@@ -7,13 +10,16 @@ public class Main {
 
 	final static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
+	static Car car = new Car();
+
 	public static void main(String[] args) {
 
 		LOGGER.info("");
-		
-		System.out.println("Learn how to commit throught git bash.");
-		System.out.println("______________________________________");
 
+		System.out.println("The colour before change: " + car.getColour());
+		Blue.makeMeBlue(car);
+		System.out.println("The colour after change: " + car.getColour());
+		
 	}
 
 }
---
+++ src/garage/Car.java
@@ -0,0 +1,25 @@
+package garage;
+
+public class Car {
+	public Car() {
+		this.colour = "black";
+	}
+
+	private String colour;
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	
+	
+
+	}
+
+	
+
+	
+	
